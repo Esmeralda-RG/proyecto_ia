5b6cfa90d4a68b7c0a7ac89a2e344089cb6542f4
code3743 <jota.lopez@correounivalle.edu.co>
2024-10-16 13:07:03 -0500

    Refactor main.dart to replace TreeViewPage with ConfigScreen and remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:graphview/GraphView.dart';
-import 'algorithms/uniform_cost.dart' as uniform_cost;
-import 'algorithms/greedy_search.dart' as greedy_search;
+import 'package:proyecto_ia/screens/config_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,127 +9,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        home: TreeViewPage(),
-      );
-}
-
-class TreeViewPage extends StatefulWidget {
-  const TreeViewPage({super.key});
-
-  @override
-  State<TreeViewPage> createState() => _TreeViewPageState();
-}
-
-class _TreeViewPageState extends State<TreeViewPage> {
-  late greedy_search.GreedySearch algorithm;
-  final streamController = StreamController<greedy_search.Node>();
-  final Graph graph = Graph()..isTree = true;
-  final BuchheimWalkerConfiguration builder = BuchheimWalkerConfiguration();
-  final paint = Paint()
-    ..color = Colors.green
-    ..strokeWidth = 1
-    ..style = PaintingStyle.stroke;
-
-  Future<void> renderNode(greedy_search.Node node) async {
-    await Future.delayed(Duration(milliseconds: 1000));
-
-    if (node.father == null) {
-      graph.addNode(Node.Id(node));
-    } else {
-      final child = Node.Id(node);
-      final parent = Node.Id(node.father);
-      graph.addEdge(parent, child);
-    }
-    streamController.add(node);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    algorithm = greedy_search.GreedySearch(
-      board: [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 0, 0],
-        [0, 0, 0, 1]
-      ],
-      advanceOrders: [
-        [-1, 0], // Arriba
-        [0, 1], // Derecha
-        [1, 0], // Abajo
-        [0, -1], // Izquierda,
-      ],
-      startX: 2,
-      startY: 0,
-      goalX: 1,
-      goalY: 3,
-    );
-
-    algorithm.search(renderNode);
-
-    builder
-      ..siblingSeparation = (50)
-      ..levelSeparation = (50)
-      ..subtreeSeparation = (50)
-      ..orientation = (BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: InteractiveViewer(
-            constrained: false,
-            boundaryMargin: EdgeInsets.all(100),
-            minScale: 0.01,
-            maxScale: 5.6,
-            child: StreamBuilder<greedy_search.Node>(
-                stream: streamController.stream,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return CircularProgressIndicator();
-                  }
-
-                  if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot.error}'),
-                    );
-                  }
-
-                  return GraphView(
-                    graph: graph,
-                    algorithm: BuchheimWalkerAlgorithm(
-                        builder, TreeEdgeRenderer(builder)),
-                    paint: paint,
-                    builder: (Node node) {
-                      final value = node.key?.value as greedy_search.Node;
-                      return nodeWidget(value,
-                          isRoot: value.father == null,
-                          isGoal: value.x == algorithm.goalX &&
-                              value.y == algorithm.goalY);
-                    },
-                  );
-                })));
-  }
-
-  Widget nodeWidget(greedy_search.Node node,
-      {bool isRoot = false, bool isGoal = false}) {
-    final color = isRoot
-        ? Colors.green.shade100
-        : isGoal
-            ? Colors.red.shade100
-            : Colors.blue.shade100;
-
-    return InkWell(
-      onTap: () {
-        print(algorithm.getPath(node));
-      },
-      child: Container(
-        padding: EdgeInsets.all(16),
-        decoration:
-            BoxDecoration(borderRadius: BorderRadius.circular(4), color: color),
-        child: Text(node.toString()),
-      ),
+    return MaterialApp(
+      title: 'Proyecto IA',
+      home: const ConfigScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
