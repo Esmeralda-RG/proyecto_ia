2854a12ba2e5c8eddbed9dc485c63055bc33aab5
code3743 <jota.lopez@correounivalle.edu.co>
2024-10-25 08:50:51 -0500

    Refactor breadth_first_search and greedy_search algorithms

--- lib/algorithms/greedy_search.dart
+++ lib/algorithms/greedy_search.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:collection/collection.dart' show PriorityQueue;
 import 'package:proyecto_ia/algorithms/search_algorithm.dart';
 
@@ -7,7 +6,7 @@ class Node implements Comparable<Node> {
   final int cost;
   final Node? father;
   final int level;
-  final int orderInLevel; 
+  final int orderInLevel;
 
   Node(this.x, this.y, this.cost, this.level, this.orderInLevel, [this.father]);
 
@@ -25,15 +24,16 @@ class Node implements Comparable<Node> {
   }
 
   @override
-  String toString() => '($x, $y) -> Costo: $cost, Nivel: $level, Orden: $orderInLevel';
+  String toString() =>
+      '($x, $y) -> Costo: $cost, Nivel: $level, Orden: $orderInLevel';
 }
 
 class GreedySearch implements SearchAlgorithm<Node> {
   final List<List<int>> board;
   final List<List<int>> advanceOrders;
   final int startX, startY, goalX, goalY;
-  int orderCounter = 0;  
-  Map<int, int> levelOrderCounter = {};  
+  int orderCounter = 0;
+  Map<int, int> levelOrderCounter = {};
 
   GreedySearch(
       {required this.board,
@@ -47,7 +47,7 @@ class GreedySearch implements SearchAlgorithm<Node> {
   Future<Node?> search(Future<void> Function(Node, [bool]) renderNode) async {
     PriorityQueue<Node> queue = PriorityQueue<Node>();
     Set<String> visited = {};
-    Node initialNode = Node(startX, startY, 0, 0, orderCounter++);  
+    Node initialNode = Node(startX, startY, 0, 0, orderCounter++);
     queue.add(initialNode);
     await renderNode(initialNode);
 
@@ -62,14 +62,18 @@ class GreedySearch implements SearchAlgorithm<Node> {
       for (var advance in advanceOrders) {
         int newX = current.x + advance[0];
         int newY = current.y + advance[1];
-        if (current.father != null && newX == current.father!.x && newY == current.father!.y) {
-          continue;  
+        if (current.father != null &&
+            newX == current.father!.x &&
+            newY == current.father!.y) {
+          continue;
         }
         if (isValid(newX, newY, board)) {
           int heuristic = manhattanDistance(newX, newY, goalX, goalY);
           int level = current.level + 1;
-          int orderInLevel = levelOrderCounter.update(level, (value) => value + 1, ifAbsent: () => 0);
-          Node neighbor = Node(newX, newY, heuristic, level, orderInLevel, current);
+          int orderInLevel = levelOrderCounter
+              .update(level, (value) => value + 1, ifAbsent: () => 0);
+          Node neighbor =
+              Node(newX, newY, heuristic, level, orderInLevel, current);
           queue.add(neighbor);
           await renderNode(neighbor);
         }
@@ -83,7 +87,11 @@ class GreedySearch implements SearchAlgorithm<Node> {
   }
 
   bool isValid(int x, int y, List<List<int>> board) {
-    return x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] != 1;
+    return x >= 0 &&
+        x < board.length &&
+        y >= 0 &&
+        y < board[0].length &&
+        board[x][y] != 1;
   }
 
   String getPath(Node? node) {
@@ -99,4 +107,3 @@ class GreedySearch implements SearchAlgorithm<Node> {
     return 'Camino encontrado: ${path.join(' -> ')}';
   }
 }
-
