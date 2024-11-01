b1ee58b36fae794b0d8a166fe5b47903e369785d
code3743 <jota.lopez@correounivalle.edu.co>
2024-11-01 16:42:14 -0500

    Rename node_extension.dart to graph_extension.dart and add methods for deleting nodes by ID

--- lib/extensions/graph_extension.dart
+++ lib/extensions/graph_extension.dart
@@ -1,6 +1,19 @@
 import 'package:graphview/GraphView.dart';
 import 'package:proyecto_ia/models/base_node.dart';
 
+extension GraphExtension on Graph {
+  void deleteNodeById(String id) {
+    final node = nodes.firstWhere((element) => element.id == id);
+    removeNode(node);
+  }
+
+  void deleteNodesById(Iterable<String> ids) {
+    final filteredNodes =
+        nodes.where((element) => ids.contains(element.id)).toList();
+    removeNodes(filteredNodes);
+  }
+}
+
 extension NodeExtension on Node {
   String get id {
     return key?.value.toString() ?? '';
